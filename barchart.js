51338bf357350076e8a0489afc6794cca0336f85
Russell McWhae <russell.mcwhae@gmail.com>
2019-05-25 17:14:23 +0000

    Chart API called

--- barchart.js
+++ barchart.js
@@ -1,1 +1,8 @@
 // drawBarChart(data, options, element);
+const drawBarChart = function(data, options, element) {
+  getElement(element);
+}
+function getElement(element) {
+  var chartElement = document.querySelector(element); // from https://www.w3schools.com/jquery/jquery_dom_add.asp
+  chartElement.innerHTML = "Hello";
+}
