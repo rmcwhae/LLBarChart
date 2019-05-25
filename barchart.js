8902b96742cc7a7f2b22966d32542f7a332f5a91
Russell McWhae <russell.mcwhae@gmail.com>
2019-05-25 17:21:42 +0000

    Working chart width and height selection

--- barchart.js
+++ barchart.js
@@ -1,6 +1,18 @@
 // drawBarChart(data, options, element);
 const drawBarChart = function(data, options, element) {
-  getElement(element);
+  var chartElement = document.querySelector(element); // assign element to modify
+
+
+// assign chart width and height if provided
+  if(options.width !== undefined) {
+    $(chartElement).width(options.width);
+  }
+  if(options.height !== undefined) {
+    $(chartElement).height(options.height);
+  }
+
+
+
 }
 function getElement(element) {
   var chartElement = document.querySelector(element); // from https://www.w3schools.com/jquery/jquery_dom_add.asp
